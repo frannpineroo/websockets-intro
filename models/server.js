6d0b1e8a971c32cde6c5f3c3e6e3134db5040db7
frannpineroo <frannpinero@hotmail.com>
2026-02-10 02:35:02 -0300

    Ordenando mi codigo con broadcast

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { socketController } = require('../sockets/controller');
 
 
 class Server {
@@ -36,18 +37,7 @@ class Server {
 
     sockets() {
 
-        this.io.on('connection', socket => {
-
-            socket.on('disconnect', () => {
-                // console.log('Cliente desconectado');
-            })
-
-            socket.on('enviar-mensaje', ( payload, callback ) => {
-                const id = 123456;
-                callback( id );
-                this.io.emit('enviar-mensaje', payload );
-            })
-        })
+        this.io.on('connection', socketController );
 
     }
 
